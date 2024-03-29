82a0b063834466d54724754dffd7558a34f3fb6d
wiktorjedrus <wiktor.jedrus@gmail.com>
2024-03-29 13:05:27 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_KEY = '99c59395-8af8-49ac-96e0-847512ae6baa';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
